128d1f170b3066e31c2cc2a9d2298ba5e3f17276
Theusznh <matheusoliveira282408@gmail.com>
2025-09-11 11:39:58 -0300

    novo codigo

--- codigos/classes.py
+++ codigos/classes.py
@@ -1,6 +1,16 @@
 import mysql.connector as mc
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QMessageBox
+from PyQt5.uic import loadUi
+
+class usuarioChat(QWidget):
+    def __init__(self, username):#, callback):
+        super().__init__()
+        loadUi("../design/templates/contatos.ui", self)  # your .ui file with a QPushButton
+
+        self.nome_salvo.setText(username)
+        # Connect the button to shared callback with username
+        # self.pushButton.clicked.connect(lambda: callback(username))
 
 class bancoDados:
     def __init__(self):
--- codigos/dashboard.py
+++ codigos/dashboard.py
@@ -1,5 +1,6 @@
-from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QVBoxLayout
 from PyQt5.uic import loadUi
+import classes
 
 import imgs_rc  # your resources
 class TelaInicial(QMainWindow):
@@ -34,6 +35,30 @@ class TelaInicial(QMainWindow):
 
         for i, botao in enumerate(self.dashboardList):
             botao.clicked.connect(partial(self.mudarDashboard, i))
+
+        scrollArea = self.usuarios_chat
+        scrollArea.setWidgetResizable(True)
+
+        container = self.usuarios_chat.widget()
+        layout = container.layout()
+
+        # garante que tenha layout
+        if not layout:
+            print("oi")
+            newlayout = QVBoxLayout()
+            newlayout.setContentsMargins(0, 0, 0, 0)
+            newlayout.setSpacing(0)
+            container.setLayout(newlayout)
+        else:
+            layout.setContentsMargins(0, 0, 0, 0)
+            layout.setSpacing(0)
+
+        users = ["Alice", "Bob", "Charlie", "Dave", "Matheus", "Leo", "Yslan"]
+
+        # cria os botões/widgets
+        for user in users:
+            btn = classes.usuarioChat(user)
+            layout.addWidget(btn)
 
     def mudarTela(self, index):
         self.stack.setCurrentIndex(index)
